7e7fa857523f58c7be2466a4b31c1be47f520555
Joshua Dobbs <dobbsjos@gmail.com>
2019-12-10 19:10:43 -0500

    added moment.js time

--- dailyPlanner.js
+++ dailyPlanner.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
 
+// declare variables
+
+var currentDay = $('#currentDay');
+var mom = moment().format('dddd MMMM Do');
 // use moment.js to create the hour function
+
+$(currentDay).append(mom);
+
 
 // need to get into the blocks of time this is going to be variables
 
